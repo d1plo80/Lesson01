dd2893308d4e3fc16c3356dd262da9f584844220
Andrey <ryazanov.aa@yandex.ru>
2022-10-07 19:29:05 +0300

    HW lesson01

---
+++ hw_task04/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.WriteLine("Введите первое число");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int thirdNum = Convert.ToInt32(Console.ReadLine());
+
+int max = firstNum;
+
+if (max < secondNum)
+{
+    max = secondNum;
+}
+
+if (max < thirdNum)
+{
+    max = thirdNum;
+    Console.WriteLine(max);
+}
+else
+{
+    Console.WriteLine(max);
+}
---
+++ hw_task08/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int min = 0;
+while (min <= n)
+{
+    if (min % 2 == 0)
+    {
+        Console.Write(min + " ");
+        min++;
+    }
+    else min++;
+}
